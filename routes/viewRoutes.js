98ca4984d3b1c9a80ffc2cdc6663cdbb9c842592
Reiyirak <pdsmej@gmail.com>
2023-05-25 15:35:53 -0600

    Finished pagination for products page and setup for product detail

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -29,8 +29,34 @@ router.get("/products", async (req, res) => {
     const data = await response.json();
     const products = Array.isArray(data.products) ? data.products : [];
 
+    const productsPerPage = 9;
+    const currentPage = req.query.page ? parseInt(req.query.page) : 1;
+    const skip = (currentPage - 1) * productsPerPage;
+
+    const totalProducts = products.length;
+    const totalPages = Math.ceil(totalProducts / productsPerPage);
+
+    const slicedProducts = products.slice(skip, skip + productsPerPage);
+
+    // Generate the pages array for pagination
+    const pages = Array.from({ length: totalPages }, (_, index) => index + 1);
+
     // Render the products.ejs template and pass the products data
-    res.render(path.join(__dirname, "..", "views", "products.ejs"), { products });
+    res.render(path.join(__dirname, "..", "views", "products.ejs"), {
+      products: slicedProducts,
+      pages,
+      currentPage,
+      totalPages,
+    });
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});
+
+router.get("/details", async (req, res) => {
+  try {
+    res.render(path.join(__dirname, "..", "views", "details.ejs"));
   } catch (error) {
     console.log(error);
   }
@@ -39,5 +65,13 @@ router.get("/products", async (req, res) => {
 // router
 //   .route("/uploadImage")
 //   .post([authenticateUser, authorizePermissions("admin")], uploadImage);
+
+router.get("/routes", async (req, res) => {
+  try {
+    res.render(path.join(__dirname, "..", "views", "routes.ejs"));
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;
