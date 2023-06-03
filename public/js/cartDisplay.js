af711f5d6c06eb3aa371bb239c20c741f29cac57
Reiyirak <pdsmej@gmail.com>
2023-06-02 22:04:42 -0600

    Dynamic display of prices on cart completed.

--- public/js/cartDisplay.js
+++ public/js/cartDisplay.js
@@ -1,8 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   const cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
   const cartItemsList = document.getElementById("cart-items");
+  const shipping_fee = document.getElementById("shippingFee");
+  const subtotal = document.getElementById("subtotal");
+  const total = document.getElementById("total");
 
   if (cartItems.length > 0) {
+    let subtotalCart = 0;
+    let shippingfee = 50;
+    let totalCart = 0;
+
     cartItems.forEach((item) => {
       let totalItem = (item.price * item.amount).toFixed(2);
       const tr = document.createElement("tr");
@@ -29,7 +36,21 @@ document.addEventListener("DOMContentLoaded", () => {
         </td>
     `;
       cartItemsList.appendChild(tr);
+      subtotalCart += parseInt(totalItem);
     });
+
+    if (subtotalCart < 200) {
+      shipping_fee.innerText = `$${shippingfee} MXN`;
+    } else {
+      shippingfee = 0;
+      shipping_fee.innerText = `$${shippingfee} MXN`;
+    }
+    // subtotalCart = (subtotalCart).toFixed(2)
+    console.log(typeof subtotalCart)
+    subtotal.innerText = `$${subtotalCart} MXN`;
+
+    totalCart = (subtotalCart + shippingfee).toFixed(2);
+    total.innerText = `$${totalCart} MXN`;
   } else {
     const tr = document.createElement("tr");
     tr.innerHTML = `
@@ -37,11 +58,15 @@ document.addEventListener("DOMContentLoaded", () => {
           <figure class="itemside">
             <div class="aside"><img src="images/icons/empty_cart.png" class="img-sm img-fluid"></div>
             <figcaption class="info">
-              <a href="#" class="title text-dark">No items at the moment</a>
+              <a href="#" class="title text-dark">El carrito esta vacio</a>
             </figcaption>
           </figure>
         </td>
     `;
     cartItemsList.appendChild(tr);
+
+    shipping_fee.innerText = "$0 MXN";
+    subtotal.innerText = "$0 MXN";
+    total.innerText = "$0 MXN";
   }
 });
